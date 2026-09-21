08c5b1fc7c4efee9acfc7708cff042f86b2f2df5
vulnace <manoj.ramesh123@gmail.com>
2026-09-21 22:26:33 +0530

    updated starship.toml and hyprland.lua

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -309,10 +309,12 @@ local closeWindowBind = hl.bind(mainMod .. " + X", hl.dsp.window.close())
 -- closeWindowBind:set_enabled(false)
 hl.bind(mainMod .. " + M", hl.dsp.exec_cmd("command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || hyprctl dispatch 'hl.dsp.exit()'"))
 hl.bind(mainMod .. " + E", hl.dsp.exec_cmd(fileManager))
-hl.bind(mainMod .. " + V", hl.dsp.window.float({ action = "toggle" }))
+-- hl.bind(mainMod .. " + V", hl.dsp.window.float({ action = "toggle" }))
 hl.bind(mainMod .. " + SPACE", hl.dsp.exec_cmd(menu))
 hl.bind(mainMod .. " + P", hl.dsp.window.pseudo())
 hl.bind(mainMod .. " + J", hl.dsp.layout("togglesplit"))    -- dwindle only
+-- vs code
+hl.bind(mainMod .. " + V", hl.dsp.exec_cmd("/opt/VSCode-linux-x64/code"))
 
 -- Suspends
 -- hl.bind(mainMod .. " + L", hl.dsp.exec_cmd("systemctl suspend"))
